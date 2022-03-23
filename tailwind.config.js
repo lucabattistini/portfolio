adeda6e778ed6a561ca7b84183016c4a95ca3f5c
lucabattistini <hello@lucabattistini.dev>
2022-03-24 00:18:31 +0100

    :wrench: chore(setup): initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    './common/**/*.{js,ts,jsx,tsx}',
+    './features/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
