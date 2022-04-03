35cb1135393dccdf606ab7be680eed4e499b82ca
lucabattistini <hello@lucabattistini.dev>
2022-04-03 16:45:16 +0200

    :wrench: chore(webgl): react three fiber support

--- next.config.js
+++ next.config.js
@@ -4,6 +4,15 @@ const nextConfig = {
   eslint: {
     dirs: ['src/app', 'src/common', 'src/components', 'src/features', 'src/lib', 'src/pages'],
   },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(glsl|vs|fs|vert|frag)$/,
+      exclude: /node_modules/,
+      use: ['raw-loader', 'glslify-loader'],
+    });
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
